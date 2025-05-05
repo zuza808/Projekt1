0e6efeba28ff6f7c3ec0fee4ffbd8695412a511c
zuza808 <s32319@pjwstk.edu.pl>
2025-05-05 15:24:06 +0200

    error
    error in the script

---
+++ Assets/scripts/I_totally_know_how_to_code_pro_programer_100.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class I_totally_know_how_to_code_pro_programer_100 : MonoBehaviour
+{
+    public string mainWord = "default";
+    public TextMeshProUGUI mainWordText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainWordText.text = mainWord;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
